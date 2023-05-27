c6bdf999e294d03b3cf991b2e766c113d572d9ef
ida <ina@ocean.io>
2023-05-27 14:31:34 +0200

    basic of microfrontend

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   mode: "development",
@@ -8,6 +9,12 @@ module.exports = {
   plugins: [
     new HtmlWebPackPlugin({
       template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        products: "products@http://localhost:8081/remoteEntry.js",
+      },
     }),
   ],
 };
